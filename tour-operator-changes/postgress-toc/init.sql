392a614f597dc7af0bdb0db2fce444e49e91e00d
eragoh <kacpergrau@gmail.com>
2024-06-10 18:58:14 +0200

    Add random TO travel changes

--- tour-operator-changes/postgress-toc/init.sql
+++ tour-operator-changes/postgress-toc/init.sql
@@ -1,6 +1,13 @@
 CREATE TABLE OPERATION (
     id SERIAL PRIMARY KEY,
     trip VARCHAR(255),
-    room VARCHAR(255),
-    CONSTRAINT unique_trip_room UNIQUE(trip, room)
+    room_is_standard INTEGER,
+    room_is_family INTEGER,
+    room_is_apartment INTEGER,
+    room_is_studio INTEGER,
+    price INTEGER,
+    adults INTEGER,
+    children_under_3 INTEGER,
+    children_under_10 INTEGER,
+    children_under_18 INTEGER
 );
--- tour-operator-changes/toc-service/app.py
+++ tour-operator-changes/toc-service/app.py
@@ -1,11 +1,13 @@
 import psycopg2
-from random import randint as rint
+from random import randint as rint, choice as choice
 import logging
 import pika
 import sys
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import json
+import threading
+import time
 import uuid
 
 # Setup logging
@@ -17,7 +19,7 @@ stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
 rabbit_connection_params = pika.ConnectionParameters(
-    '180140_rabbitmq-gateway',
+    'rabbitmq-gateway',
     port=5672,
     credentials=pika.PlainCredentials('admin', 'password'))
 
@@ -45,15 +47,26 @@ def publish_topic_event(rabbit_connection, event, routing_key, exchange_name='or
     channel.close()
 
 
-def insert(trip, room):
+def insert(trip_id, update):
     connection = get_connection()
     cursor = connection.cursor()
+
+    room_is_standard = update.get('room', {}).get('is_standard', -1)
+    room_is_family = update.get('room', {}).get('is_family', -1)
+    room_is_apartment = update.get('room', {}).get('is_apartment', -1)
+    room_is_studio = update.get('room', {}).get('is_studio', -1)
+    price = update.get('price', -1)
+    adults = update.get('adults', -1)
+    children_under_3 = update.get('children_under_3', -1)
+    children_under_10 = update.get('children_under_10', -1)
+    children_under_18 = update.get('children_under_18', -1)
+
     cursor.execute(
         """
-        INSERT INTO OPERATION (trip, room)
-        VALUES (%s, %s)
+        INSERT INTO OPERATION (trip, room_is_standard, room_is_family, room_is_apartment, room_is_studio, price, adults, children_under_3, children_under_10, children_under_18)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """,
-        (trip, room)
+        (trip_id, room_is_standard, room_is_family, room_is_apartment, room_is_studio, price, adults, children_under_3, children_under_10, children_under_18)
     )
     connection.commit()
 
@@ -103,7 +116,6 @@ def handle_buy_request(ch, method, properties, body):
         }
         result = db.travelOffers.update_one({"_id": ObjectId(trip_id)}, update_query)
         logger.info(f'DB UPDATE: {result}')
-        insert(trip_id, room)
     except Exception as error:
         logger.info(f'Error handle_buy: {error}')
 
@@ -122,6 +134,63 @@ def handle_rooms_request(ch, method, properties, body):
         reservation_info['room'] = {}
     publish_topic_event(get_rabbit_connection(), reservation_info, 'result')
     
+last_delete_time = 0
+
+def random_mongo_updates():
+    global last_delete_time
+    while True:
+        try:
+            db = get_mongo_db()
+            # Randomly select a document
+            all_ids = [doc['_id'] for doc in db.travelOffers.find()]
+            if not all_ids:
+                logger.info('No records found to update')
+                continue
+
+            random_id = choice(all_ids)
+
+            # 10% chance to delete a record, but only if more than 2 minutes have passed since last deletion
+            if time.time() - last_delete_time > 240 and rint(1, 10) == 1:
+                db.travelOffers.delete_one({"_id": random_id})
+                logger.info(f'Deleted document: {random_id}')
+                last_delete_time = time.time()
+                # Log deletion as well
+                insert(str(random_id), {"room_is_standard": -1, "room_is_family": -1, "room_is_apartment": -1, "room_is_studio": -1, "price": -1, "adults": -1, "children_under_3": -1, "children_under_10": -1, "children_under_18": -1})
+            else:
+                # Update random fields
+                random_update = {}
+                selected_field = choice(['adults', 'children', 'price', 'room'])
+
+                if selected_field == 'adults':
+                    random_update['adults'] = rint(1, 6)  # Random number of adults
+                elif selected_field == 'children':
+                    children_under_3 = rint(0, 3)
+                    children_under_10 = rint(0, 3)
+                    children_under_18 = rint(0, 3)
+                    random_update['children'] = children_under_3 + children_under_10 + children_under_18
+                    random_update['children_under_3'] = children_under_3
+                    random_update['children_under_10'] = children_under_10
+                    random_update['children_under_18'] = children_under_18
+                elif selected_field == 'price':
+                    random_update['price'] = rint(20, 100) * 100  # Random price
+                elif selected_field == 'room':
+                    random_update['room'] = {
+                        "is_standard": rint(0, 20),
+                        "is_family": rint(0, 20),
+                        "is_apartment": rint(0, 20),
+                        "is_studio": rint(0, 20)
+                    }
+
+                update_query = {"$set": random_update}
+                result = db.travelOffers.update_one({"_id": random_id}, update_query)
+                logger.info(f'Updated document: {random_id} with {random_update}')
+                
+                # Insert the changes into PostgreSQL
+                insert(str(random_id), random_update)
+
+            time.sleep(45)
+        except Exception as error:
+            logger.error(f'Error in random_mongo_updates: {error}')
 
 def listen_to_rabbitmq():
     rabbit_connection = get_rabbit_connection()
@@ -153,6 +222,10 @@ def listen_to_rabbitmq():
         rabbit_connection.close()
 
 if __name__ == '__main__':
+    updater_thread = threading.Thread(target=random_mongo_updates)
+    updater_thread.daemon = True
+    updater_thread.start()
+
     listen_to_rabbitmq()
 
 
