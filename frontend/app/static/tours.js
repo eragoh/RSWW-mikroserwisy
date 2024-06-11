c345ce234a5daf701c092b36cb335c3cb8435404
Chlodwig-tech <szczepan.chojnowski@tutanota.com>
2024-06-11 10:51:21 +0200

    add 'Dalej' button on tours page

--- frontend/app/static/tours.js
+++ frontend/app/static/tours.js
@@ -27,6 +27,9 @@ export default{
 
             this.countries = await (await fetch('/getcountries/')).json();
         },
+        nextpage: function(){
+            window.location.href = '/tours?page=' + (Number(this.page) + 1)
+        },
         redirectToTour(url) {
             window.location.href = url;
         },
@@ -164,64 +167,70 @@ export default{
 
         
             <div class="row justify-content-center mb-3" v-if="tours" v-for="tour in tours">
-            <div class="col-md-12 col-xl-10">
-                <div class="card shadow-0 border rounded-3">
-                <div class="card-body">
-                    <div class="row">
-                    <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
-                        <div class="bg-image hover-zoom ripple rounded ripple-surface">
-                        <a :href="tour._id.$oid"><img :src="tour.img" class="w-100" /></a>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-6 col-xl-6">
-                        <h5>
-                            <i class="bi bi-house"></i> 
-                            <a :href="tour._id.$oid" style="text-decoration: none;">&nbsp{{tour.hotel}}</a>
-                        </h5>
-                        <div class="d-flex flex-row">
-                        <div class="text-danger mb-1 me-2" v-for="star in tour.score">
-                            <i class="bi bi-star-fill"></i>                            
-                        </div>
-                        <div class="text-danger mb-1 me-2" v-for="star in 5 - tour.score">
-                            <i class="bi bi-star"></i>                            
-                        </div>
-                        <span>
-                            {{ tour.country }}{{ tour.city !== '' ? ', ' + tour.city : '' }}
-                        </span>
-                        </div>
-                        <div class="mt-1 mb-0 text-muted small">
-                            <i class="bi bi-calendar-range"></i> {{tour.start_date}} - {{tour.end_date}}<br />
-                            <i class="bi bi-airplane"></i> {{tour.departure_location}}<br />
-                            <div style="display: inline-flex; align-items: center;">
-                                <i class="bi bi-houses"></i>
-                                <div style="margin-left: 8px;" class="mb-2 text-muted small" v-html="roomsf(tour.room)"></div>
+                <div class="col-md-12 col-xl-10">
+                    <div class="card shadow-0 border rounded-3">
+                    <div class="card-body">
+                        <div class="row">
+                        <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
+                            <div class="bg-image hover-zoom ripple rounded ripple-surface">
+                            <a :href="tour._id.$oid"><img :src="tour.img" class="w-100" /></a>
                             </div>
                         </div>
-                        <p class="text-truncate mb-4 mb-md-0">
-                        {{tour.description}}
-                        </p>
-                    </div>
-                    <div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
-                        <div class="d-flex flex-row align-items-center mb-1">
-                        <h4 class="mb-1 me-1">{{tour.price}}zł/os.</h4>
-                        <span class="text-danger"><s>{{Math.round(1.2 * tour.price) + 0.99}}zł</s></span>
+                        <div class="col-md-6 col-lg-6 col-xl-6">
+                            <h5>
+                                <i class="bi bi-house"></i> 
+                                <a :href="tour._id.$oid" style="text-decoration: none;">&nbsp{{tour.hotel}}</a>
+                            </h5>
+                            <div class="d-flex flex-row">
+                            <div class="text-danger mb-1 me-2" v-for="star in tour.score">
+                                <i class="bi bi-star-fill"></i>                            
+                            </div>
+                            <div class="text-danger mb-1 me-2" v-for="star in 5 - tour.score">
+                                <i class="bi bi-star"></i>                            
+                            </div>
+                            <span>
+                                {{ tour.country }}{{ tour.city !== '' ? ', ' + tour.city : '' }}
+                            </span>
+                            </div>
+                            <div class="mt-1 mb-0 text-muted small">
+                                <i class="bi bi-calendar-range"></i> {{tour.start_date}} - {{tour.end_date}}<br />
+                                <i class="bi bi-airplane"></i> {{tour.departure_location}}<br />
+                                <div style="display: inline-flex; align-items: center;">
+                                    <i class="bi bi-houses"></i>
+                                    <div style="margin-left: 8px;" class="mb-2 text-muted small" v-html="roomsf(tour.room)"></div>
+                                </div>
+                            </div>
+                            <p class="text-truncate mb-4 mb-md-0">
+                            {{tour.description}}
+                            </p>
                         </div>
-                        <h6 class="text-success">Dostępny</h6>
-                        <div class="d-flex flex-column mt-4">
-                        
-                        <button @click="redirectToTour(tour._id.$oid)" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-sm" type="button">Dalej</button>
-                        <button @click="redirectToReservation(tour._id.$oid)" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-primary btn-sm mt-2" type="button">
-                            Kup teraz!
-                        </button>
+                        <div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
+                            <div class="d-flex flex-row align-items-center mb-1">
+                            <h4 class="mb-1 me-1">{{tour.price}}zł/os.</h4>
+                            <span class="text-danger"><s>{{Math.round(1.2 * tour.price) + 0.99}}zł</s></span>
+                            </div>
+                            <h6 class="text-success">Dostępny</h6>
+                            <div class="d-flex flex-column mt-4">
+                            
+                            <button @click="redirectToTour(tour._id.$oid)" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-sm" type="button">Dalej</button>
+                            <button @click="redirectToReservation(tour._id.$oid)" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-primary btn-sm mt-2" type="button">
+                                Kup teraz!
+                            </button>
+                            </div>
+                        </div>
                         </div>
                     </div>
                     </div>
                 </div>
-                </div>
-            </div>
             </div>
             <div v-else>
                 😔 Brak dostępnych wycieczek o podanych parametrach. 😔
+            </div>
+
+            <div class="row justify-content-center mb-3" v-if="tours">
+                <button @click="nextpage()" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-primary btn-sm mt-2" type="button">
+                    Dalej
+                </button>
             </div>
 
 
